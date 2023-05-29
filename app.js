e509b73b90fdb0f941a3d9232d92b223edafccd8
Adam <hviidadam@gmail.com>
2023-05-29 22:17:38 +0200

    cleanup

--- app.js
+++ app.js
@@ -34,13 +34,6 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 app.post("/form", upload.fields([{ name: "image" }, { name: "cv" }]), (req, res) => {
-  const { name, email, country } = req.body;
-  const imageFile = req.files["image"][0];
-  const cvFile = req.files["cv"][0];
-
-  console.log({ name: name, email: email, country: country });
-  console.log({ imageFile: imageFile, cvFile: cvFile });
-
   res.send({ data: req.body, files: req.files });
 });
 
